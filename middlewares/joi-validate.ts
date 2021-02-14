0322119ab1f2d8ae35a9b5691b7fe5cb629fdfdf
daylonblakely <daylonblakely@gmail.com>
2021-02-14 12:45:31 -0800

    Updates

--- middlewares/joi-validate.ts
+++ middlewares/joi-validate.ts
@@ -1,7 +1,8 @@
-import { Request, Response, NextFunction } from "express";
-import Joi from "joi";
-import { JoiRequestValidationError } from "../errors/joi-validation-error";
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+import { JoiRequestValidationError } from '../errors/joi-validation-error';
 
+// validates an object as a joi schema
 export const validateSchema = (schema: Joi.Schema) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req.body);
